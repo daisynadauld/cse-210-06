2e80df9d74f09a7ab4949a2805bc17eae00c6b94
Daisy Nadauld <92403835+daisynadauld@users.noreply.github.com>
2022-02-12 23:29:40 -0700

    Anna changes

--- game/directing/director.py
+++ game/directing/director.py
@@ -3,16 +3,19 @@ It loops through the input, updates, and output phases until
 the game ends."""
 
 from game.shared.point import Point
+from game.services.keyboard_service import KeyboardService
+from game.casting.score import Score
 import random
+import raylib
 
-class Director:
+class Director():
     """A person who directs the game. 
     
     The responsibility of a Director is to control the sequence of play.
 
     Attributes:
         _keyboard_service (KeyboardService): For getting directional input.
-        _video_service (VideoService): For providing video output.
+        _video_service (VideoService): For providing video output
     """
 
     def __init__(self, keyboard_service, video_service):
@@ -22,11 +25,12 @@ class Director:
             keyboard_service (KeyboardService): An instance of KeyboardService.
             video_service (VideoService): An instance of VideoService.
         """
+        
         self._keyboard_service = keyboard_service
         self._video_service = video_service
-        self._total_score = 0
+        self._score = 0
         
-    def start_game(self, cast):
+    def start_game(self, cast, keyboard_service, score):
         """Starts the game using the given cast. Runs the main game loop.
 
         Args:
@@ -34,46 +38,41 @@ class Director:
         """
         self._video_service.open_window()
         while self._video_service.is_window_open():
-            self._get_inputs(cast)
-            self._do_updates(cast)
+            self._get_inputs(keyboard_service)
+            self._do_updates(cast, score)
             self._do_outputs(cast)
         self._video_service.close_window()
 
-    def _get_inputs(self, cast):
+    def _get_inputs(self, keyboard_service):
         """Gets directional input from the keyboard and applies it to the robot.
         
         Args:
             cast (Cast): The cast of actors.
         """
-        robot = cast.get_first_shape("robots")
-        velocity = self._keyboard_service.get_direction()
+        self._keyboard_service = keyboard_service.get_mouse_input()
 
-        robot.set_velocity(velocity)        
-
-    def _do_updates(self, cast):
+    def _do_updates(self, cast, score):
         """Updates the robot's position and resolves any collisions with artifacts.
         
         Args:
             cast (Cast): The cast of shapes.
         """
         banner = cast.get_first_shape("banners")
-        robot = cast.get_first_shape("robots")
-        minerals = cast.get_shapes("minerals")
-
-        banner.set_text("Score: {}".format(self._total_score))
+        coins = cast.get_shapes("coins")
+        banner.set_text(f"Score: {self._score}")
         max_x = self._video_service.get_width()
         max_y = self._video_service.get_height()
-        robot.move_next(max_x, max_y)
+        direction = self._keyboard_service
         
-        for mineral in minerals:
-            
-            mineral.move_next(max_x, max_y)
-            
-            if robot.get_position().equals(mineral.get_position()):
-                self._total_score = mineral.calc_score(self._total_score)
-                banner.set_text("Score: {}".format(self._total_score))
-                mineral._position = Point(random.randint(1, max_x), 0)
-                
+        
+        for coin in coins:
+            coin.move_next(max_x, max_y)
+            if direction.__eq__(coin.get_position()):
+                cast.remove_shape("coins", coin)
+                self._score = score.add_points()
+                banner.set_text(f"Score: {self._score}")
+                coin._position = Point(random.randint(1, max_x), 0)
+
 
     def _do_outputs(self, cast):
         """Draws the shapes on the screen.
